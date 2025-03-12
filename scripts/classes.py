50d66ee539b798ecf193cf0dfb16d18d0e71506e
Alice Ferreira <alice.fandrade@a.ucb.br>
2025-03-12 11:54:53 -0300

    correct: classes implementation

--- scripts/classes.py
+++ scripts/classes.py
@@ -1,31 +1,22 @@
 from abc import ABC, abstractmethod
 
-class NaturalPerson:
+class Customer:
 
-    def __init__(self, cpf, name, birth_date):
-        self._cpf = cpf
-        self._name = name
-        self._birth_date = birth_date
+    def __init__(self, address, accounts):
+        self._address = address
+        self._accounts = accounts
 
+    def make_transaction(account, transaction):
+        pass
 
-class CheckingAccount:
-
-    def __init__(self, limit, withdraw_limit):
-        self._limit = limit
-        self._withdraw_limit = withdraw_limit
-
-
-class Transaction(ABC):
-
-    @abstractmethod
-    def record(account: Account):
+    def add_account(account):
         pass
 
 
-class Account(CheckingAccount):
+class Account():
 
-    def __init__(self, id, balance, agency, customer, history):
-        self._id = id
+    def __init__(self, account_id, balance, agency, customer, history):
+        self._id = account_id
         self._balance = balance
         self._agency = agency
         self._customer = customer
@@ -34,7 +25,7 @@ class Account(CheckingAccount):
     def balance() -> float:
         pass
     
-    def new_account(self, customer: Customer) -> Account:
+    def new_account(self, customer):
         pass
 
     def withdraw(value: float) -> bool:
@@ -44,17 +35,32 @@ class Account(CheckingAccount):
         pass
 
 
-class Customer(NaturalPerson):
+class Transaction(ABC):
 
-    def __init__(self, address, accounts):
-        self._address = address
-        self._accounts = accounts
-
-    def make_transaction(account: Account, transaction: Transaction):
+    @abstractmethod
+    def record(account):
         pass
 
-    def add_account(account: Account):
+
+class History:
+
+    def add_transaction(transaction):
         pass
+
+
+class NaturalPerson(Customer):
+
+    def __init__(self, cpf, name, birth_date):
+        self._cpf = cpf
+        self._name = name
+        self._birth_date = birth_date
+
+
+class CheckingAccount(Account):
+
+    def __init__(self, limit, withdraw_limit):
+        self._limit = limit
+        self._withdraw_limit = withdraw_limit
 
 
 class Deposit(Transaction):
@@ -72,10 +78,4 @@ class Withdrawal(Transaction):
         self._value = value
 
     def record():
-        pass
-
-
-class History:
-
-    def add_transaction(transaction: Transaction):
         pass
